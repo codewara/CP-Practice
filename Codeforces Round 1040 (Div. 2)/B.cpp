30eaf7335fb7a2e7da5bfbe9f2461ffac662fcb3
Daneswara <150428419+codewara@users.noreply.github.com>
2025-08-01 04:17:45 +0700

    B - Codeforces 1040-2

--- Codeforces Round 1040 (Div. 2)/B.cpp
+++ Codeforces Round 1040 (Div. 2)/B.cpp
@@ -1,6 +1,9 @@
 /*
 Codeforces Round 1040 (Div. 2)
 [B] Pathless (https://codeforces.com/contest/2130/problem/B)
+
+Solved by   : codewara
+Solved date : 01-08-2025
 */
 
 #include <bits/stdc++.h>
@@ -8,70 +11,20 @@ Codeforces Round 1040 (Div. 2)
 #define skibidi ios_base::sync_with_stdio(false);
 #define dop cin.tie(NULL);
 #define dob cout.tie(NULL);
-#define fi first
-#define se second
-#define lf long double
 #define int long long
-#define mp make_pair
-#define pb push_back
-#define qi queue<int>
 #define vi vector<int>
-#define vvi vector<vi>
-#define pii pair<int, int>
-#define atv(x, y) (x).begin() + (y)
-#define all(x) (x).begin(), (x).end()
 #define rep(i, x, y) for (int i = (x); i < (y); i++)
 #define per(i, x, y) for (int i = (x); i >= (y); i--)
 using namespace std;
 
-const int INF = 1e18;
-const int MOD = 1e9 + 7;
-const int MAX = LLONG_MAX;
-const int MIN = LLONG_MIN;
-
 void solve () {
-    int n, s; cin >> n >> s;
-    vi a(n);
-    int sum = 0, c1 = 0, c2 = 0;
-    rep(i, 0, n) {
-        cin >> a[i];
-        sum += a[i];
-        if (a[i] == 1) c1++;
-        if (a[i] == 2) c2++;
-    }
+    int n, x, s; cin >> n >> s;
+    vi a(3); rep(i, 0, n) cin >> x, a[x]++;
 
-    if (s == sum) {
-        cout << -1 << endl;
-        return;
-    } else if (s < sum) {
-        rep (i, 0, n) cout << a[i] << (i == n - 1 ? "\n" : " ");
-        return;
-    }
-
-    vi comb(n, 0);
-    if ((s - sum) % 3 != 0) {
-        comb[n - 1] = 2; comb[n - 2] = 1;
-        c2--; c1--;
-    } else if ((s - sum) % 2 != 0 && c1 >= 2) {
-        comb[n - 1] = 1; comb[n - 2] = 1;
-        c1 -= 2;
-    } else if ((s - sum) % 4 == 0 && c2 >= 2) {
-        comb[n - 1] = 2; comb[n - 2] = 2;
-        c2 -= 2;
-    } else {
-        cout << -1 << endl;
-        return;
-    }
-
-    per (i, n - 3, 0) {
-        if (c2 > 0) {
-            comb[i] = 2;
-            c2--;
-        } else if (c1 > 0) {
-            comb[i] = 1;
-            c1--;
-        }
-    } rep(i, 0, n) cout << comb[i] << (i == n - 1 ? "\n" : " ");
+    if (s == a[1] + 2 * a[2] || s >= a[1] + 2 * a[2] + 2) { cout << -1 << endl; return; }
+    rep (i, 0, a[0]) cout << 0 << ' ';
+    rep (i, 0, a[2]) cout << 2 << ' ';
+    rep (i, 0, a[1]) cout << 1 << (i == a[1] - 1 ? '\n' : ' ');
 }
 
 signed main () {
